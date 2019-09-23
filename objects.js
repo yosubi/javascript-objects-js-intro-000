b586e8fbcb694fd7f93c56c56f65e38820532e9f
Joseph Moon <nakkta@gmail.com>
2019-09-23 21:31:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  return Object.assign(playlist, {[artistName]: songTitle})
 }
