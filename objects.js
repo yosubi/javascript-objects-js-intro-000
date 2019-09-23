f08fb0ad7d5fbb19e39a8e972870b3de289bae5d
Joseph Moon <nakkta@gmail.com>
2019-09-23 21:35:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistNAme
+  delete playlist[artistNAme]
   return playlist
 }
