797c81aa2913bca6ccbd5eb852607cb84d205b0b
Joseph Moon <nakkta@gmail.com>
2019-09-23 21:21:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,16 +1,3 @@
 var playlist = {
-  George Benson: "Breezin",
-  'Mel Torme': "When Sunny Gets Blue"
-}
-
-function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song
-
-  return playlist
-}
-
-function removeFromPlaylist(playlist, artist) {
-  delete playlist[artist]
-
-  return playlist
+  
 }
