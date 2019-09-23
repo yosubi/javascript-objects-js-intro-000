d8167c649af8921a149354fe38b17ded0ebb7503
Joseph Moon <nakkta@gmail.com>
2019-09-23 21:30:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,7 @@
 var playlist = {
   'Mel Torme':'When Sunny Gets Blue'
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+}
